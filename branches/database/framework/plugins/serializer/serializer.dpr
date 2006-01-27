7799c4ce1332c8ae71502caf0c2fa5ff8485fac0
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2006-01-27 22:48:08 +0000

    modification serialization

--- branches/database/framework/plugins/serializer/serializer.dpr
+++ branches/database/framework/plugins/serializer/serializer.dpr
@@ -2,7 +2,11 @@ library serializer;
 
 uses
   plugintf,
-  serializerclasses in 'serializerclasses.pas';
+  serializerclasses in 'serializerclasses.pas',
+  DAC_TLB in 'xmlcursor\dac_tlb.pas',
+  MSXML2_TLB in 'xmlcursor\msxml2_tlb.pas',
+  StdXML_TLB in 'xmlcursor\stdxml_tlb.pas',
+  XMLCursor in 'xmlcursor\xmlcursor.pas';
 
 function NewPlugin: IPlugUnknown;
 begin
