d11457b8c80943fb8bc00146766b72600849621d
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2006-10-11 14:21:30 +0000

    correction des gui.glade

--- trunk/windows/Sofia.Core/Model/Dossier.cs
+++ trunk/windows/Sofia.Core/Model/Dossier.cs
@@ -6,21 +6,15 @@ namespace Sofia.Core.Model
 	
 	public class Dossier
     {
-    	string folderId;
         string caption;
         DateTime creation;
         
-        public Dossier(string folderId, DateTime creation, string caption)
+        public Dossier(DateTime creation, string caption)
         {
-        	this.folderId = folderId;
         	this.creation = creation;
             this.caption = caption;            
         }
-        
-        public string FolderId { 
-        	get { return folderId; } 
-        }
-        
+      	
         public string Caption { 
         	get { return caption; } 
         }
