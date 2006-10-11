b8a50c682f265818a478151d249d8ea6d1be67bf
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2006-10-11 16:17:00 +0000

    mise à jour de la classe document

--- trunk/src/Sofia.Core/Model/Document.cs
+++ trunk/src/Sofia.Core/Model/Document.cs
@@ -7,8 +7,26 @@ namespace Model
 	/// </summary>
 	public class Document
 	{
-		public Document()
-		{
-		}
+		string caption;
+        DateTime creation;
+        
+        public Document(DateTime creation, string caption)
+        {
+        	this.creation = creation;
+            this.caption = caption;            
+        }
+      	
+        public string Caption { 
+        	get { return caption; } 
+        }
+        
+        public DateTime Creation { 
+        	get { return creation; } 
+        }
+        
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", caption, creation);
+        }
 	}
 }
