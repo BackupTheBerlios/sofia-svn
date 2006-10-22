0668acc7866a58bf46fcce6cdd95f5e551cd1f9f
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2006-10-22 21:49:24 +0000

    ajout des icones dans QuickView

---
+++ trunk/src/Sofia.Views/Sofia.Views.Quick/QuickViewGui/StockIcons.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+namespace Sofia.Views.QuickView
+{
+	
+	public class Stock
+	{
+		public static readonly string RecentIcon = Gtk.Stock.New;
+		public static readonly string FavoritesIcon = Gtk.Stock.About;
+		public static readonly string TrashIcon = Gtk.Stock.Delete;
+	}
+	
+}
