505cbf43f2788ffa3c5bc7185ed8030781c7990e
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2006-11-03 18:54:53 +0000

    affichage des dossiers

--- trunk/src/Sofia.Core/ViewController/TreeViewBuilder.cs
+++ trunk/src/Sofia.Core/ViewController/TreeViewBuilder.cs
@@ -11,95 +11,74 @@ namespace Sofia.Core
 	public class TreeViewBuilder
 	{
 		
-		internal const int TextColumn = 0;
-		internal const int OpenIconColumn = 1;
-		internal const int ClosedIconColumn = 2;
-		internal const int DataItemColumn = 3;
-		internal const int FilledColumn = 4;
-		
 		private Gtk.TreeView _TreeView;
-		private Gtk.TreeStore _TreeStore;
-		
-		internal Gtk.TreeViewColumn _CompleteColumn;
-		internal Gtk.CellRendererText _TextRender;
-		
-		Hashtable _Icons;
+		private Gtk.TreeStore _TreeStore; 
 				
+		Gtk.TreeViewColumn _ObjectColumn;
+		
+		Gtk.CellRendererText _ObjectRenderer;
 		
 		public TreeViewBuilder (Gtk.TreeView treeView)
 		{
-			_Icons = new Hashtable ();
 			_TreeView = treeView;	
-			_TreeStore = new Gtk.TreeStore (typeof (string), typeof (Gdk.Pixbuf), typeof (Gdk.Pixbuf), typeof (object), typeof(bool));
+			_TreeStore = new Gtk.TreeStore (typeof (Gdk.Pixbuf),	typeof (Dossier));
 			
-			_TreeView.HeadersVisible = false;
-			_TreeView.SearchColumn = 0;
-			_TreeView.EnableSearch = true;
-			
-			_CompleteColumn = new Gtk.TreeViewColumn ();
-			_CompleteColumn.Title = "column";
+			SetRenderers();
+			SetColumns();
 
-			Gtk.CellRendererPixbuf pixRender = new Gtk.CellRendererPixbuf ();
-			_CompleteColumn.PackStart (pix_render, false);
-			_CompleteColumn.AddAttribute (pix_render, "pixbuf", OpenIconColumn);
-			_CompleteColumn.AddAttribute (pix_render, "pixbuf-expander-open", OpenIconColumn);
-			_CompleteColumn.AddAttribute (pix_render, "pixbuf-expander-closed", ClosedIconColumn);
-
-			_TextRender = new Gtk.CellRendererText ();
-				
-			_CompleteColumn.PackStart (_TextRender, true);
-			_CompleteColumn.AddAttribute (_TextRender, "markup", TextColumn);
-	
-			_TreeView.AppendColumn (_CompleteColumn);
-					
 			SetDataFunctions();
 			
-			treeView.Model = treeStore;
+			_TreeView.Model = _TreeStore;
  		}
 		
-
-		public Gdk.Pixbuf GetIcon (string id)
+		private void SetRenderers()
 		{
-			Gdk.Pixbuf icon = _Icons [id] as Gdk.Pixbuf;
-			if (icon == null) {
-				icon = _TreeView.RenderIcon (id, Gtk.IconSize.Menu, "");
-				_Icons [id] = icon;
-			}
-			return icon;
+			_ObjectRenderer = new Gtk.CellRendererText ();
+		}
+		
+		private void SetColumns()
+		{
+		    _TreeView.AppendColumn ("Icon", new Gtk.CellRendererPixbuf (), "pixbuf", 0);  
+			_ObjectColumn =  _TreeView.AppendColumn ("Document", _ObjectRenderer, "text", 1);			
 		}
 		
 		private void SetDataFunctions()
 		{
-			objectColumn.SetCellDataFunc (objectRenderer, new Gtk.TreeCellDataFunc (RenderObject));
+			_ObjectColumn.SetCellDataFunc (_ObjectRenderer, new Gtk.TreeCellDataFunc (RenderObject));
 		}
 		
 		public void Fill(IList dossiers)
-		{			
+		{
+			//Gtk.Image folderImage = new Gtk.Image(Gtk.Stock.Directory, Gtk.IconSize.Menu);
 			Gdk.Pixbuf folderImage = Gdk.Pixbuf.LoadFromResource("stock-folder.png");
 			Gdk.Pixbuf documentImage = Gdk.Pixbuf.LoadFromResource("stock-generic.png");
 			
 			foreach (Dossier dossier in dossiers) 
 			{				
-				Gtk.TreeIter iter = treeStore.AppendValues(folderImage, dossier);
+				Gtk.TreeIter iter = _TreeStore.AppendValues(folderImage, dossier);
 				
 				foreach (Document document in dossier.Documents) 
 				{
 					//TODO : Déterminer l'icone en fonction du document					
-					treeStore.AppendValues(iter, documentImage, document);
-					Console.WriteLine("////////////////////////////////////// caption du doc : " + document.Caption);
+					_TreeStore.AppendValues(iter, documentImage, document);
 				}
 			}
 		}
 		
 		private void RenderObject (Gtk.TreeViewColumn column, Gtk.CellRenderer cell, Gtk.TreeModel model, Gtk.TreeIter iter)
 		{
-			if (model.IterHasChild(iter)) {
-				Dossier dossier = (Dossier) model.GetValue (iter, 1);
+			object obj = model.GetValue (iter, 1);
+			Type dossierType = typeof (Dossier);
+			Type documentType = typeof (Document);
+			
+			if ( dossierType.IsInstanceOfType(obj) ) {
+				Dossier dossier = (Dossier) obj;
  				(cell as Gtk.CellRendererText).Text = dossier.Caption;
- 			} else {
- 				Document document = (Document) model.GetValue(iter, 1);
- 				(cell as Gtk.CellRendererText).Text = document.Caption;
- 			}
+ 			} else 
+ 			    if ( documentType.IsInstanceOfType(obj) ) {
+ 					Document document = (Document) obj;
+ 					(cell as Gtk.CellRendererText).Text = document.Caption;
+ 				}
 		}		
 		
 	}
