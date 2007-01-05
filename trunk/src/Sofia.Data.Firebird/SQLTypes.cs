b5dc20c2c46dc2dfe32ce0e39afd250c0e3c9d54
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2007-01-05 17:21:33 +0000

    classes spécifiques firebird

---
+++ trunk/src/Sofia.Data.Firebird/SQLTypes.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+using Sofia.Data.Common;
+
+namespace Sofia.Data.Firebird
+{
+    public static class SQLTypes: ISQLTypes
+    {
+        public static string GetTypeString(DbType dbType)
+        {
+            return "";
+        }
+    }
+}
