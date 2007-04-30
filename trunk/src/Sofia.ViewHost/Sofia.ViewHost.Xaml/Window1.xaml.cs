fd09f6111d25e729566fb203cf7c2c92c5a66dba
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2007-04-30 14:25:31 +0000

    New framework

--- trunk/src/Sofia.ViewHost/Sofia.ViewHost.Xaml/Window1.xaml.cs
+++ trunk/src/Sofia.ViewHost/Sofia.ViewHost.Xaml/Window1.xaml.cs
@@ -7,10 +7,12 @@ using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Navigation;
+using Sofia.Plugins;
+using Sofia.DesignPatterns;
 
 namespace Sofia
 {
-	public partial class Window1
+  public partial class Window1 : IViewHost, IObserver
 	{
 		public Window1()
 		{
@@ -18,5 +20,38 @@ namespace Sofia
 			
 			// Insert code required on object creation below this point.
 		}
-	}
+
+    #region IObserver Members
+
+    public void Update(object sender, object notification)
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    #endregion
+
+    #region IViewHost Members
+
+    public void ShowView(Sofia.Mvc.IView view, Sofia.Mvc.ViewDestination destination)
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    public void ShowToolBar(Sofia.Mvc.IView view, int row)
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    public void Save()
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    public void New()
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    #endregion
+  }
 }
