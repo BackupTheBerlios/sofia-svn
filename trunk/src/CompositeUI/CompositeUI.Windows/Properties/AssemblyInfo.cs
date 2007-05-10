3f43da076937dc403b8bf1e12283a07487b3e761
neurosoup <neurosoup@920f2255-9a0a-0410-995b-f75540858897>
2007-05-10 20:27:00 +0000

    import WPF CAB

--- trunk/src/CompositeUI/CompositeUI.Windows/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Windows.Markup;
-
-//map CLR namespaces to XML namespace
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/cab/wpf", "Microsoft.Practices.CompositeUI.Windows")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/cab/wpf", "Microsoft.Practices.CompositeUI.Windows.Workspaces")]
